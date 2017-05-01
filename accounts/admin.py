237224395cfa53781721f726b9acb179c2839a24
icnbrave <fuzhiyong19861210@163.com>
2017-05-01 22:23:38 +0800

    add gender and address field to accounts.admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-	fields=('user', 'nickname', 'is_email_verified', 'is_phone_verified', 'is_deleted', 'img_heading')
+	fields=('user', 'nickname', 'gender', 'addr', 'is_email_verified', 'is_phone_verified', 'is_deleted', 'img_heading')
 	readonly_fields=('user', 'is_email_verified', 'is_phone_verified')
 	save_on_top=True
 

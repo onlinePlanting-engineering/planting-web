a7184b89ff8414aec9c374b094428eb15c8081d6
icnbrave <fuzhiyong19861210@163.com>
2017-05-01 18:14:41 +0800

    remove un-usable imports.

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework_expiring_authtoken import views
 
 from .views import (
     UserCreateAPIView,
-    UserListAPIView,
     UserLogoutAPIView,
     UserViewSet,
     reset_password
@@ -21,12 +20,11 @@ user_detail = UserViewSet.as_view({
 })
 
 urlpatterns = [
-    url(r'^$', UserListAPIView.as_view(), name='list'),
+    url(r'^$', user_list, name='list'),
     url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
     url(r'^login/$', views.obtain_expiring_auth_token, name='login'),
     url(r'^logout/$', UserLogoutAPIView.as_view(), name='logout'),
     url(r'^get_auth_token/$', views.obtain_expiring_auth_token, name='get_auth_token'),
     url(r'^reset_password', reset_password, name='reset_password'),
-    url(r'^(?P<pk>[0-9]+)/$', user_detail, name='user_detail'),
-    # url(r'^?P(<pk>[0-9]+)/edit/$', UserDetailView.as_view(), name='user_update')
+    url(r'^(?P<pk>[0-9]+)/$', user_detail, name='detail'),
 ]

cd551b0fb0bec17ce7517b26f9be792c0c74661d
icnbrave <fuzhiyong19861210@163.com>
2017-05-08 20:28:40 +0800

    1. package return data; 2. categorize models

--- farm/api/serializers.py
+++ farm/api/serializers.py
@@ -5,21 +5,24 @@ from accounts.api.serializers import (
     UserFilteredPrimaryKeyRelatedField
 )
 
+
+class FarmImageSerializer(serializers.ModelSerializer):
+    # Create a custom method field, that list farms belong to current user
+    # farm = UserFilteredPrimaryKeyRelatedField(queryset=Farm.objects, source='farm.name')
+
+    class Meta:
+        model = FarmImage
+        fields = ('url', 'image', 'flags', 'is_delete')
+
 class FarmSerializer(serializers.ModelSerializer):
-    images = serializers.HyperlinkedRelatedField(many=True, view_name='farmimage-detail', read_only=True)
+    # images = serializers.HyperlinkedRelatedField(many=True, view_name='farmimage-detail', read_only=True)
+    images = FarmImageSerializer(many=True, read_only=True)
     owner = serializers.ReadOnlyField(source='owner.username')
     notice = serializers.HyperlinkedIdentityField(view_name='farm-notice', format='html')
     content = serializers.HyperlinkedIdentityField(view_name='farm-content', format='html')
 
     class Meta:
-        model = Farm
         fields = ('url', 'id', 'name', 'owner', 'price', 'subject',
                   'is_delete', 'notice', 'content', 'images')
 
-class FarmImageSerializer(serializers.ModelSerializer):
-    # Create a custom method field, that list farms belong to current user
-    farm = UserFilteredPrimaryKeyRelatedField(queryset=Farm.objects, source='farm.name')
-
-    class Meta:
-        model = FarmImage
-        fields = ('id', 'url', 'farm', 'image', 'flags', 'is_delete')
+        model = Farm
--- seeds/api/views.py
+++ seeds/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from seeds.models import Category, Vegetable, VegMeta, VegMetaImage
@@ -17,12 +17,44 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def list(self, request, *args, **kwargs):
+        categories = Category.objects.all().order_by('-id')
+        serializer = self.get_serializer(categories, many=True)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+
 class VegetableViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This API just provide list and retrieve specific vegetable information
     """
     queryset = Vegetable.objects.all()
     serializer_class = VegetableSerializer
+
+    def list(self, request, *args, **kwargs):
+        vegetables = Vegetable.objects.all().order_by('-id')
+        serializer = self.get_serializer(vegetables, many=True)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
 
 class VegMetaViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -31,9 +63,41 @@ class VegMetaViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = VegMeta.objects.all()
     serializer_class = VegMetaSerializer
 
+    def list(self, request, *args, **kwargs):
+        vegmetas = VegMeta.objects.all().order_by('-id')
+        serializer = self.get_serializer(vegmetas, many=True)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+
 class VegMetaImageViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This API just provide list and retrieve specific vegetable information
     """
     queryset = VegMetaImage.objects.all()
     serializer_class = VegMetaImageSerializer
+
+    def list(self, request, *args, **kwargs):
+        vegmetaimages = VegMetaImage.objects.all().order_by('-id')
+        serializer = self.get_serializer(vegmetaimages, many=True)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'data': serializer.data,
+            'status_code': status.HTTP_200_OK
+        }, status=status.HTTP_200_OK)

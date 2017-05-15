253c5c8c9291e4e9f7e171256002929864977af3
icnbrave <fuzhiyong19861210@163.com>
2017-05-15 22:31:04 +0800

    1. corrent addprofile script

--- accounts/management/commands/addprofile.py
+++ accounts/management/commands/addprofile.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 def add_user_profile():
     users = User.objects.filter(profile=None)
     for user in users:
-        Profile.objects.get_or_create(user=user)
+        Profile.objects.get_or_create(owner=user)
 
 class Command(BaseCommand):
     help = 'Create profile for those who has not profile yet.'

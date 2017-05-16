eb400fbd98b582da25415344785d1cd81dedf43e
icnbrave <fuzhiyong19861210@163.com>
2017-05-16 22:42:07 +0800

    change media root back to project directory

--- planting/settings.py
+++ planting/settings.py
@@ -142,7 +142,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..','static/images/upload')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/images/upload')
 MEDIA_URL = '/media/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
